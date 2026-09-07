5d05e77c0115ef28b4f53517cb97573c3997d08b
szabika-pvp <szatomi2008@gmail.com>
2026-09-07 08:07:38 +0200

    Create GUI

--- src/main/java/hu/szatomi/diakok/MainController.java
+++ src/main/java/hu/szatomi/diakok/MainController.java
@@ -1,14 +1,19 @@
 package hu.szatomi.diakok;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    public Button allButton;
+    public Button sandorButton;
+    public Button musketeerButton;
+    public Button yearButton;
+    public Button classButton;
+    public ListView<String> resultList;
+    public Button saveButton;
+
 }
