323cd728b1f7248ebbe511c5b724bfd93d10127b
szabika-pvp <szatomi2008@gmail.com>
2026-09-07 08:24:47 +0200

    Load students

--- src/main/java/hu/szatomi/diakok/MainController.java
+++ src/main/java/hu/szatomi/diakok/MainController.java
@@ -1,19 +1,72 @@
 package hu.szatomi.diakok;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.VBox;
 
-public class MainController {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
 
     public Button allButton;
     public Button sandorButton;
     public Button musketeerButton;
     public Button yearButton;
     public Button classButton;
-    public ListView<String> resultList;
+    public ListView<Student> resultList;
     public Button saveButton;
+
+    private final ArrayList<Student> students = new ArrayList<>();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        File file = new File("diakok.csv");
+        loadStudents(file);
+
+        resultList.getItems().addAll(students);
+    }
+
+    private void loadStudents(File file) {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            reader.lines().forEach(line -> {
+
+                String[] data = line.split(";");
+
+                Integer id = Integer.valueOf(data[0]);
+                String lastName = data[1];
+                String firstName = data[2];
+                String sex = data[3];
+                String studentClass = data[4];
+                String birthDate = data[5];
+                String city = data[6];
+                Integer height = Integer.valueOf(data[7]);
+
+                students.add(new Student(id, lastName, firstName, sex, studentClass, birthDate, city, height));
+            });
+
+        } catch (FileNotFoundException e)  {
+            showError(e);
+        }
+    }
+
+    private void showError(Exception e) {
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Hiba");
+        alert.setHeaderText(e.getMessage());
+        alert.showAndWait();
+    }
 
 }
---
+++ src/main/java/hu/szatomi/diakok/Student.java
@@ -0,0 +1,17 @@
+package hu.szatomi.diakok;
+
+public record Student(Integer id,
+                      String lastName,
+                      String firstName,
+                      String sex,
+                      String studentClass,
+                      String birthDate,
+                      String city,
+                      Integer height
+) {
+
+    @Override
+    public String toString() {
+        return "%s %s, %s (%s %s, %d cm)".formatted(lastName, firstName, studentClass, city, birthDate, height);
+    }
+}
